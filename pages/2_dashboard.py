127db090bc39d634da921d31be64969e052f6eff
Anggun17-hub <acunaanggunfilisia@students.unnes.ac.id>
2025-05-22 09:09:22 +0700

    Create 2_dashboard.py

---
+++ pages/2_dashboard.py
@@ -0,0 +1,12 @@
+import streamlit as st 
+import pandas as pd
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.matriks import classification_report, accuracy_score
+
+st.set_page_config(page_title="prediction")
+st.tittle("prediction")
+st.header("prediction")
+
+#load dataset
+df
